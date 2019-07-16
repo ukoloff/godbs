4f440ec033ec6368cc4f2405323dcf6338a39955
Stas Ukolov <ukoloff@gmail.com>
2019-07-16 16:36:24 +0500

    Opimize save code

--- dbs/save.go
+++ dbs/save.go
@@ -24,6 +24,20 @@ func (me *dbsWriter) init(from io.Writer) {
 	me.writer = bufio.NewWriter(from)
 }
 
+// Write binary data
+func (me *dbsWriter) write(data interface{}) {
+	binary.Write(me.writer, binary.LittleEndian, data)
+}
+
+// Write DBS record
+func (me *dbsWriter) startRecord(recType int16, payload int) {
+	var rec recAny
+	rec.Type = recType
+	rec.ID = me.id
+	rec.beforeWrite(payload)
+	me.write(rec)
+}
+
 // Write DBS file
 func (me *dbsWriter) writeDBS(dbs *DBS) {
 	for _, part := range *dbs {
@@ -50,75 +64,56 @@ func (me *dbsWriter) writePart(part *Part) {
 
 // Write Rec1 for Path
 func (me *dbsWriter) rec1(path *Path, partID int16) {
-	var prolog recAny
 	var epilog rec2
 
 	me.id++
-	prolog.ID = me.id
-	prolog.Type = 1
-	prolog.beforeWrite(binary.Size(epilog) + binary.Size(rec1item{})*len(*path))
+	me.startRecord(1, binary.Size(epilog)+binary.Size(rec1item{})*len(*path))
+
 	epilog.Subtype = 1
 	epilog.Part = partID
 	epilog.Original = me.id
 	epilog.RecO2.eye()
-
-	binary.Write(me.writer, binary.LittleEndian, prolog)
-	binary.Write(me.writer, binary.LittleEndian, epilog)
+	me.write(epilog)
 
 	for _, node := range *path {
 		var nodeRec rec1item
 		nodeRec.fromNode(&node)
-		binary.Write(me.writer, binary.LittleEndian, nodeRec)
+		me.write(nodeRec)
 	}
 }
 
 // Write Rec8 for Part
 func (me *dbsWriter) rec8(count int) {
-	var prolog recAny
-	prolog.ID = me.id
-	prolog.Type = 8
-	prolog.beforeWrite(binary.Size(rec8item{}) * count)
-	binary.Write(me.writer, binary.LittleEndian, prolog)
+	me.startRecord(8, binary.Size(rec8item{})*count)
 
 	for i := 0; i < count; i++ {
-		ref := rec8item{ID: int16(int(me.id) - count + i)}
-		binary.Write(me.writer, binary.LittleEndian, ref)
+		me.write(rec8item{ID: int16(int(me.id) - count + i)})
 	}
 }
 
 // Write Rec26 for Part
 func (me *dbsWriter) rec26(part *Part) {
-	var prolog recAny
 	var epilog rec26
-	prolog.ID = me.id
-	prolog.Type = 26
-	prolog.beforeWrite(binary.Size(epilog))
-	binary.Write(me.writer, binary.LittleEndian, prolog)
+	me.startRecord(26, binary.Size(epilog))
 
 	epilog.fromString(part.Name)
-
-	binary.Write(me.writer, binary.LittleEndian, epilog)
+	me.write(epilog)
 }
 
 // Write Rec27 for Part
 func (me *dbsWriter) rec27(part *Part) {
-	var prolog recAny
 	var epilog rec27
-	prolog.ID = me.id
-	prolog.Type = 27
-	prolog.beforeWrite(binary.Size(epilog))
-	binary.Write(me.writer, binary.LittleEndian, prolog)
+	me.startRecord(27, binary.Size(epilog))
 
 	epilog.Area = float32(part.Area() / 1e4)
 	epilog.Perimeter = float32(part.Perimeter() / 1e2)
-
-	binary.Write(me.writer, binary.LittleEndian, epilog)
+	me.write(epilog)
 }
 
 // Write EOF
 func (me *dbsWriter) writeEOF() {
 	var eof recEOF
 	eof.init()
-	binary.Write(me.writer, binary.LittleEndian, eof)
-	defer me.writer.Flush()
+	me.write(eof)
+	me.writer.Flush()
 }
--- dbs/save_test.go
+++ dbs/save_test.go
@@ -5,11 +5,23 @@ import (
 	"testing"
 )
 
-func TestSave(t *testing.T) {
-	dbs := NewCircle(1)
-	// var out bytes.Buffer
-	// dbs.Save(&out)
-	// fmt.Println(out.Bytes())
+func TestSaveCircle(t *testing.T) {
+	dbs := NewCircle(99)
 	f, _ := os.Create("testdata/.!.dbs")
+	defer f.Close()
+	dbs.Save(f)
+}
+
+func TestSaveRect(t *testing.T) {
+	dbs := NewRect(&Point{27, 42})
+	f, _ := os.Create("testdata/.r.dbs")
+	defer f.Close()
+	dbs.Save(f)
+}
+
+func TestSaveSquare(t *testing.T) {
+	dbs := NewSquare(108)
+	f, _ := os.Create("testdata/.q.dbs")
+	defer f.Close()
 	dbs.Save(f)
 }
