23712b919804c835fa31f7e25344a4c1d820bf33
Stas Ukolov <ukoloff@gmail.com>
2019-07-17 14:57:56 +0500

    Several untested Geometry methods

--- dbs/o2.go
+++ dbs/o2.go
@@ -10,21 +10,47 @@ type O2 struct {
 }
 
 // Eye makes transformation matrix identity
-func (me *O2) Eye() {
+func (me *O2) Eye() *O2 {
 	*me = O2{}
 	me.X.X = 1
 	me.Y.Y = 1
+	return me
+}
+
+// CCW initialize matrix to rotation
+func (me *O2) CCW(angle float64) *O2 {
+	sin, cos := math.Sincos(angle * math.Pi / 180.0)
+	me.X = Point{cos, sin}
+	me.Y = Point{-sin, cos}
+	me.Delta = Point{}
+	return me
+}
+
+// Shift makes pure translation transformation
+func (me *O2) Shift(delta Point) *O2 {
+	me.Eye()
+	me.Delta = delta
+	return me
+}
+
+// Compose combines several transformation (right to left!)
+func (me *O2) Compose(others ...*O2) *O2 {
+	var p3 [3]Point
+	p3[0].X = 1
+	p3[1].Y = 1
+	for i := len(others) - 1; i >= 0; i-- {
+		t := others[i]
+		for j := range p3 {
+			p3[j] = p3[j].Apply(t)
+		}
+	}
+	me.X = p3[0].Sub(&p3[2])
+	me.Y = p3[1].Sub(&p3[2])
+	me.Delta = p3[2]
+	return me
 }
 
 // Det calculates determinant of transforamtion matrix
 func (me *O2) Det() float64 {
 	return me.X.X*me.Y.Y - me.X.Y*me.Y.X
-}
-
-// CCW initialize matrix to rotation
-func (me *O2) CCW(angle float64) {
-	sin, cos := math.Sincos(angle * math.Pi / 180.0)
-	me.X = Point{cos, sin}
-	me.Y = Point{-sin, cos}
-	me.Delta = Point{}
 }
--- dbs/span.go
+++ dbs/span.go
@@ -1,6 +1,9 @@
 package dbs
 
-import "math"
+import (
+	"math"
+	"math/cmplx"
+)
 
 // Span of contour - line or arc
 type Span struct {
@@ -37,4 +40,81 @@ func (span *Span) Perimeter() float64 {
 			(1 + square(span.Bulge))
 	}
 	return p
+}
+
+// linear is internal function to find Points in local coordinates
+func (span *Span) linear(pos complex128) Point {
+	return C2Point(((span.Z.C()-span.A.C())*pos + (span.Z.C() + span.A.C())) / 2)
+}
+
+// Zenith is a middle point of arc
+func (span *Span) Zenith() Point {
+	return span.linear(complex(0, -span.Bulge))
+}
+
+// Nadir is a point opposite to a middle point of arc (not on arc itself)
+func (span *Span) Nadir() Point {
+	return span.linear(complex(0, 1/span.Bulge))
+}
+
+// Center returns center of an arc
+func (span *Span) Center() Point {
+	return span.linear(complex(0, (1/span.Bulge-span.Bulge)/2))
+}
+
+// Radius returns that of an arc
+func (span *Span) Radius() float64 {
+	return math.Abs(1/span.Bulge+span.Bulge) / 4 * span.Vector().Abs()
+}
+
+// At finds position of a Point on the Arc
+//	-1: Start of Arc
+// 	 0: Middle of Arc
+//  +1: End of Arc
+func (span *Span) At(pos float64) Point {
+	return span.linear(
+		complex(pos, -span.Bulge) /
+			complex(1, -pos*span.Bulge))
+}
+
+// AtUniform is like At, returns position of Point on Arc, but slightly more uniform
+// (Especially for arcs with big Bulge)
+func (span *Span) AtUniform(pos float64) Point {
+	q := (math.Sqrt(9+8*square(span.Bulge)) + 1) / 4
+	return span.At(pos / (q - (q-1)*square(pos)))
+}
+
+// PositionOf finds position for a Point on the Arc
+// (Reverse of At)
+func (span *Span) PositionOf(point *Point) float64 {
+	a := point.Sub(&span.A).Abs()
+	z := point.Sub(&span.Z).Abs()
+	return (a - z) / (a + z)
+}
+
+// tgq - helper (static) function: tan(arg(sqrt(vector)))
+func (span *Span) tgq(vector complex128) float64 {
+	if real(vector) < 0 {
+		return (cmplx.Abs(vector) - real(vector)) / imag(vector)
+	}
+	return imag(vector) / (cmplx.Abs(vector) + real(vector))
+}
+
+// BulgeOf finds bulge for an arc that passes thru a Point
+func (span *Span) BulgeOf(point *Point) float64 {
+	return span.tgq(
+		cmplx.Conj(point.Sub(&span.A).C()) *
+			span.Z.Sub(point).C())
+}
+
+// LeftBulge finds Bulge for sub-arc from start to that position
+func (span *Span) LeftBulge(pos float64) float64 {
+	return span.tgq(
+		complex(1, span.Bulge) *
+			complex(1, pos*span.Bulge))
+}
+
+// RightBulge finds Bulge for sub-arc from that position to end
+func (span *Span) RightBulge(pos float64) float64 {
+	return span.LeftBulge(-pos)
 }
